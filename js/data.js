f7771608d9f0a75dc7f9ce2d9584470fa304f856
Gleb Dolgushin <dolgushing@gmail.com>
2019-06-28 19:46:53 +0300

    правки

--- js/data.js
+++ js/data.js
@@ -21,10 +21,10 @@
     var offerKeys = Object.keys(window.data.offers);
     for (var i = 0; i < numberOfAdvertisements; i++) {
       advertisements[i] = {
-        author: { avatar: 'img/avatars/user0' + (i + 1) + '.png' },
-        offer: { type: offerKeys[window.util.generateRandomNumber(offerKeys.length)] },
+        author: {avatar: 'img/avatars/user0' + (i + 1) + '.png'},
+        offer: {type: offerKeys[window.util.generateRandomNumber(offerKeys.length)]},
         // Задаём расположение острого конца метки
-        location: { x: window.util.generateRandomNumber(pinMaxX) + PIN_WIDTH / 2, y: PIN_MIN_Y + window.util.generateRandomNumber(PIN_MAX_Y - PIN_MIN_Y) + PIN_HEIGHT }
+        location: {x: window.util.generateRandomNumber(pinMaxX) + PIN_WIDTH / 2, y: PIN_MIN_Y + window.util.generateRandomNumber(PIN_MAX_Y - PIN_MIN_Y) + PIN_HEIGHT}
       };
     }
     return advertisements;
